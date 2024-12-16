f5db6c3701af490866a1fd87e44a8ab66d0b7135
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-16 19:42:35 +0530

    Update 8bitCPU.v

--- 8bitCPU.v
+++ 8bitCPU.v
@@ -2,7 +2,7 @@
 module alu_8bit (a ,b ,alu_sel ,alu_out ,carry_out);
 input [7:0] a, b;
 input [2:0] alu_sel;
-output reg [7:0] alu_out;
+output reg [7:0] alu_out;// reg???????????????what da hell?
 output reg carry_out;
     
 always @(*) begin
@@ -65,7 +65,7 @@ endmodule
 //control unit module
 module control_unit (opcode,alu_sel);
 input [2:0] opcode;
-output reg [2:0] alu_sel;
+    output reg [2:0] alu_sel;//reg?????????
 
 always @(*)
     begin
