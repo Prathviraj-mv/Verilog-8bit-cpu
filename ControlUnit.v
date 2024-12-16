64b931d2033d5d6ece182d7606cadc5e9ffd285f
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-16 20:54:25 +0530

    Update ControlUnit.v

--- ControlUnit.v
+++ ControlUnit.v
@@ -1,1 +1,21 @@
+//control unit module
+module control_unit (opcode,alu_sel);
+input [2:0] opcode;
+output reg [2:0] alu_sel;//reg?????????
 
+always @(*)
+    begin
+    case (opcode)
+        3'b000: alu_sel = 3'b000;//add
+        3'b001: alu_sel = 3'b001;//sub
+        3'b010: alu_sel = 3'b010;//and
+        3'b011: alu_sel = 3'b011;//or
+        3'b100: alu_sel = 3'b100;//exor
+        3'b101: alu_sel = 3'b101;//multiple
+        3'b110: alu_sel = 3'b110;//div
+        3'b111: alu_sel = 3'b111;//compare
+        default: alu_sel = 3'b000;//add
+    endcase
+end
+
+endmodule
