4bad66a15d1363ca562274a6e0f3989d2b3f913c
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-18 16:53:20 +0530

    Update 8BITInstructions.v

--- modules/8BITInstructions.v
+++ modules/8BITInstructions.v
@@ -1,12 +1,14 @@
 
-module instruction_memory (a,b,alu_sel,result_out,carry_out,clk,rst,data_in,data_out);
+module instruction_memory (a,b,opcode,alu_sel,result_out,carry_out,clk,rst,data_out);
 
   
   input wire [7:0] a;       // Operand A (8 bits)
   input wire [7:0] b;       // Operand B (8 bits)
-  input wire [2:0] alu_sel;    // ALU Operation select signal
+  input wire [2:0] opcode;    // ALU Operation select signal
   input wire clk; //clock pulse for the registers
   input wire rst; // reset for the registers
+
+  output wire [2:0] alu_sel;     // Output of ALU //results
   output wire [7:0] result_out;     // Output of ALU //results
   output wire [7:0] carry_out ;  // CARRY out values
   output wire [7:0] data_out;// data stored in the registers
