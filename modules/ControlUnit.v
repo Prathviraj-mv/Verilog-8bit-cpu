357cb2a6209e1ff71b08335233aa734c662614c1
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-18 20:17:37 +0530

    Update ControlUnit.v

--- modules/ControlUnit.v
+++ modules/ControlUnit.v
@@ -7,15 +7,15 @@ reg [2:0] alu_sel;
     always @(opcode)
     begin
     case (opcode)
-        3'b000: alu_sel = 3'b000;//add
-        3'b001: alu_sel = 3'b001;//sub
+        3'b000: alu_sel = 3'b000;//addition
+        3'b001: alu_sel = 3'b001;//subtraction
         3'b010: alu_sel = 3'b010;//and
         3'b011: alu_sel = 3'b011;//or
         3'b100: alu_sel = 3'b100;//exor
-        3'b101: alu_sel = 3'b101;//multiple
-        3'b110: alu_sel = 3'b110;//div
-        3'b111: alu_sel = 3'b111;//compare
-        default: alu_sel = 3'b000;//add
+        3'b101: alu_sel = 3'b101;//multiplication
+        3'b110: alu_sel = 3'b110;//divison
+        3'b111: alu_sel = 3'b111;//comparison
+        default: alu_sel = 3'b000;//addition as default
     endcase
 end
 
