f5dfca1bc53753347e409fddc1c232a66d868108
PrateekVasista <prateek.n.vasista@gmail.com>
2024-12-19 20:50:09 +0530

    Update ALU_tb.v

--- testbench/ALU_tb.v
+++ testbench/ALU_tb.v
@@ -10,7 +10,16 @@ module alu_8bit_tb;
   alu_8bit uut(a,b,alu_sel,alu_out,carry_out);
   initial
     begin
+      a=01101111;b=01101111;
+      alu_sel=3'b000;#10;
+      alu_sel=3'b001;#10;
+      alu_sel=3'b010;#10;
+      alu_sel=3'b011;#10;
+      alu_sel=3'b100;#10;
+      alu_sel=3'b101;#10;
+      alu_sel=3'b110;#10;
+      alu_sel=3'b111;#10;
+    end
+endmodule
       
-  
-      alu_sel=3'd0;
       
