c251ccad26964c86ed584b6aa35fe4954c4abe82
PrateekVasista <prateek.n.vasista@gmail.com>
2024-12-19 19:57:03 +0530

    Update ControlUnit_tb.v

--- testbench/ControlUnit_tb.v
+++ testbench/ControlUnit_tb.v
@@ -1,19 +1,21 @@
+// control unit test bench
+
 module control_unit_tb(opcode,alu_sel);
-  reg opcode;
-  wire alu_sel;
+   reg opcode;
+   wire alu_sel;
 
   control_unit  uut(opcode,alu_sel);
-  initial
-    begin
-       opcode=3'b000;#10; //add
-       opcode=3'b001;#10; //sub
-       opcode=3'b010;#10; //and
-       opcode=3'b011;#10; //or
-       opcode=3'b100;#10; //xor
-       opcode=3'b101;#10; //product
-       opcode=3'b110;#10; //div
-       opcode=3'b111;#10; //compare
-    end
+      initial
+           begin
+                 opcode=3'b000;#10; //add
+                 opcode=3'b001;#10; //sub
+                 opcode=3'b010;#10; //and
+                 opcode=3'b011;#10; //or
+                 opcode=3'b100;#10; //xor
+                 opcode=3'b101;#10; //product
+                 opcode=3'b110;#10; //div
+                 opcode=3'b111;#10; //compare
+           end
 endmodule
 
 
