0c02fb9f8a814152d598408811dc4a95a3767fba
PrateekVasista <prateek.n.vasista@gmail.com>
2024-12-19 19:51:44 +0530

    Update ControlUnit_tb.v

--- testbench/ControlUnit_tb.v
+++ testbench/ControlUnit_tb.v
@@ -1,1 +1,22 @@
+module control_unit_tb(opcode,alu_sel);
+  reg opcode;
+  wire alu_sel;
+
+  control_unit  uut(opcode,alu_sel);
+  initial
+    begin
+      opcode=3'b000;#10;
+       opcode=3'b001;#10;
+       opcode=3'b010;#10;
+       opcode=3'b011;#10;
+       opcode=3'b100;#10;
+       opcode=3'b101;#10;
+       opcode=3'b110;#10;
+       opcode=3'b111;#10;
+    end
+endmodule
+
+
+
+
 
