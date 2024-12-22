804330f17c745be73bc53b0669c57f8d59a095fb
PrateekVasista <prateek.n.vasista@gmail.com>
2024-12-22 15:03:23 +0530

    Update registers_tb.v

--- testbench/registers_tb.v
+++ testbench/registers_tb.v
@@ -1,7 +1,7 @@
 
 // control unit test bench
 
-module register_module (save,reset,alu_out,data_out);
+module register_module_tb; 
   reg save;
   reg reset;
   reg [7:0] alu_out;
@@ -11,7 +11,21 @@ module register_module (save,reset,alu_out,data_out);
   register_module  uut(save,reset,alu_out,data_out);
       initial
            begin
-            
+             save=0;
+             reset=0;
+             alu_out=8'b00000000;
+             //Apply reset
+             #10 reset = 1;
+             #10 reset = 0;
+
+             //Save first value
+             alu_out = 8'b10101010;
+             #10 save = 1;
+             #10 save = 0;
+
+             //Applying reset again
+             #10 reset = 1;
+             #10 reset = 0;
            end
 endmodule
 
