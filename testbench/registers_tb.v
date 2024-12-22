8b252774373444dd15e9558c8aa71d1bc822b73a
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-22 14:39:32 +0530

    Update registers_tb.v

--- testbench/registers_tb.v
+++ testbench/registers_tb.v
@@ -1,1 +1,21 @@
 
+// control unit test bench
+
+module register_module (save,reset,alu_out,data_out);
+  reg save;
+  reg reset;
+  reg [7:0] alu_out;
+  wire  [7:0] data_out;
+  wire  [7:0] data_out;
+
+  register_module  uut(save,reset,alu_out,data_out);
+      initial
+           begin
+            
+           end
+endmodule
+
+
+
+
+
