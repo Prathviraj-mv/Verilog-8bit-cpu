7883d8ad55399cb2b11f89be5417694aa738b860
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-22 10:21:27 +0530

    Update 8BITInstructions_tb.v

--- testbench/8BITInstructions_tb.v
+++ testbench/8BITInstructions_tb.v
@@ -12,35 +12,43 @@ module instruction_memory_tb;
     wire[2:0] alu_sel;
     instruction_memory uut (a,b,opcode,alu_sel,result_out,carry_out,clk,rst,data_out);
 
-    // // Clock generation
-    // initial 
-    //   begin
-    //     forever
-    //     clk = 0;#10;
-    //     clk = ~clk;#10; // Toggle clock every 5 time units
-    // end
+    // Clock generation
+    initial 
+      begin
+        forever
+        clk = 0;#10;
+        clk = ~clk;#10; 
+    end
 
     initial
       begin
 
-        // Test Case 1: Addition (a + b)
-        a = 8'b00000101; // 5
-        b = 8'b00000011; // 3
-        opcode = 3'b000; // Add
-        clk = 1;
-        rst = 0;
-        #10;
+        //addition (a + b)
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b000;clk =1;rst=0;#20;
+          
+        //sub (a-b)
+        a = 8'b11001100;b = 8'b10101010;opcode = 3'b001;clk =1;rst=0;#20;
+          
+        //and
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b010;clk =1;rst=0;#20;
+          
+        //or
+        a = 8'b11001100;b = 8'b10101010;opcode = 3'b011;clk =1;rst=0;#20;
+
+        //xor
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b100;clk =1;rst=0;#20; 
+          
+        //mul
+        a = 8'b11001100;b = 8'b10101010;opcode = 3'b101;clk =1;rst=0;#20;
+
+        //div  
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b110;clk =1;rst=0;#20;
+          
+        //comp
+        a = 8'b11001100;b = 8'b10101010;opcode = 3'b111;clk =1;rst=0;#20;
 
         
-        // Test Case 2: AND operation (a&b)
-        a = 8'b11001100;
-        b = 8'b10101010;
-        opcode = 3'b010; // AND
-        clk =1;
-        rst =0;
-        #10;
 
-        
 
     end
 
