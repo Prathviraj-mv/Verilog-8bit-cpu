5e47f79d48a18e0c379fd19bcf728da5b41e3827
chappie <168354749+Prathviraj-mv@users.noreply.github.com>
2024-12-27 11:47:14 +0530

    Update cpu_tb.v

--- 8BITCPU+CLOCK/cpu_tb.v
+++ 8BITCPU+CLOCK/cpu_tb.v
@@ -13,39 +13,43 @@ module instruction_memory_tb;
 
     
     instruction_memory uut (a,b,opcode,alu_sel,alu_out,carry_out,clk,reset,data_out);
-
+    initial 
+        begin forever
+            clk =0;#10;
+            clk =~clk;#10;
+        end   
 
 
     initial
       begin
 
         //add
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b000;save =0;reset=1;#20;
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b000;reset=0;#20;
 
         //sub
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b001;save=1;reset=0;#20;
-        reset =1;#20;
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b001;reset=0;#20;
+        
         
           
         //and
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b010;save=1;reset=0;#20;
-        reset =1;#20;
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b010;reset=0;#20;
+       
 
         //or
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b011;save=1;reset=0;#20;
-        reset =1;#20;
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b011;reset=0;#20;
+       
 
         //xor
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b100;save =1;reset=0;#20; 
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b100;reset=0;#20; 
  
         //mul
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b101;save=1;reset=0;#20;
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b101;reset=0;#20;
 
         //div  
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b110;save=1;reset=0;#20;
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b110;reset=0;#20;
 
         //comp
-        a = 8'b00000101;b = 8'b00000011;opcode = 3'b111;save=1;reset=0;#20;
+        a = 8'b00000101;b = 8'b00000011;opcode = 3'b111;reset=0;#20;
 
 
 
